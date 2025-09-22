4f82b641e5a61741ef0e5d6249168ce60eff12af
yano-static <yanopalanas@gmail.com>
2025-09-22 19:11:41 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// Centered on UE Manila
+const map = L.map('map', {
+  minZoom: 16,
+  maxZoom: 20,
+}).setView([14.6070, 121.0040], 17);
+
+// Add OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// Limit the map view around UE campus bounds
+const ueBounds = [
+  [14.6050, 121.0020], // Southwest corner
+  [14.6090, 121.0060]  // Northeast corner
+];
+map.setMaxBounds(ueBounds);
+map.on('drag', function() {
+  map.panInsideBounds(ueBounds, { animate: true });
+});
+
+// Example marker for Lost & Found Center
+L.marker([14.6070, 121.0040])
+  .addTo(map)
+  .bindPopup("<b>UE Manila</b><br>Lost & Found Center");
