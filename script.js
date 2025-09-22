baeaa7c63a0e4943fdbea9889b4b665123a27155
yano-static <yanopalanas@gmail.com>
2025-09-22 18:55:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Example interactivity
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Website loaded!");
+  
+  // Example: show message in map
+  const mapDiv = document.getElementById("map");
+  mapDiv.innerText = "Interactive map will go here!";
+});
