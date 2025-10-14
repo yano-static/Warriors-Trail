0e5dbb412480cc3cee44e57752c6517c92378f64
yano-static <yanopalanas@gmail.com>
2025-10-14 10:22:36 +0800

    Create firebase-config.js
    feat: add firebase-config.js with project credentials

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCyJwAGiPmDWl6tYhm_nV_EJMkoZIrxKXg",
+  authDomain: "warriorstrail-9c05b.firebaseapp.com",
+  databaseURL: "https://warriorstrail-9c05b-default-rtdb.firebaseio.com",
+  projectId: "warriorstrail-9c05b",
+  storageBucket: "warriorstrail-9c05b.firebasestorage.app",
+  messagingSenderId: "187592091783",
+  appId: "1:187592091783:web:cb8ce312d1026e0f4fb022",
+  measurementId: "G-0LQHP3VVZN"
+};
